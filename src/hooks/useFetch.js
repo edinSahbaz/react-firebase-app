73982cb3c6a6a34d15da8a684d3580b76c5c9b1f
edin.sahbaz <edin.sahbaz@hotmail.com>
2022-08-22 15:46:29 +0200

    Posts page

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,42 @@
+import { useState, useEffect } from "react";
+
+const useFetch = (url) => {
+  const [data, setData] = useState(null);
+  const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    const abortContr = new AbortController();
+
+    fetch(url, { signal: abortContr.signal })
+      .then((res) => {
+        if (!res.ok) {
+          throw Error("could not fetch the data");
+        }
+        return res.json();
+      })
+      .then((data) => {
+        setData(data);
+        setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        if (err.name === "AbortError") {
+          console.log("fetch aborted");
+        } else {
+          setIsPending(false);
+          setError(err.message);
+        }
+      });
+
+    return () => abortContr.abort();
+  }, [url]);
+
+  return {
+    data,
+    isPending,
+    error,
+  };
+};
+
+export default useFetch;
--- src/pages/Posts/Posts.js
+++ src/pages/Posts/Posts.js
@@ -1,5 +1,38 @@
+import "./Posts.css";
+import useFetch from "../../hooks/useFetch";
+
 const Posts = () => {
-  return <h1>Posts</h1>;
+  const {
+    data: posts,
+    isPending,
+    error,
+  } = useFetch("https://jsonplaceholder.typicode.com/posts");
+
+  return (
+    <div className="content">
+      <h1>Posts</h1>
+      <p className="description">
+        Posts page to display data.
+        <br />
+        100 posts from JSON palcehoder API.
+      </p>
+
+      <div>{isPending && <h3>Loading posts...</h3>}</div>
+
+      <div>{error && <h3>Error: {error}</h3>}</div>
+
+      <div className="posts">
+        {posts &&
+          posts.map((post) => (
+            <div className="post">
+              <p>Post #{post.id}</p>
+              <h3>{post.title}</h3>
+              <p>{post.body}</p>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
 };
 
 export default Posts;
