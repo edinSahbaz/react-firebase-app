bdc5a8b7f315d828510dede3eadbcfda2d4bc898
edin.sahbaz <edin.sahbaz@hotmail.com>
2022-08-29 21:56:48 +0200

    Changes

--- src/Components/AuthError/AuthError.js
+++ src/Components/AuthError/AuthError.js
@@ -4,7 +4,7 @@ const AuthError = () => {
   return (
     <div className="content">
       <h2>Please log in or register!</h2>
-      <p>You must be logged in to see this page.</p>
+      <p>You must be registered / logged in to see this page.</p>
       <Link to="/" className="btn">
         Register / Log In Form
       </Link>
